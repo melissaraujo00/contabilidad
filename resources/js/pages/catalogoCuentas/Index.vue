6d61d6f6f164b73653207d6d3d41edf8357d17f6
Luis Zelaya <codingwithluis@github.com>
2025-11-25 17:08:40 -0600

    Added: Tabla Shadcn para mostrar datos en CatalogoCuenta/Index.vue

--- resources/js/pages/catalogoCuentas/Index.vue
+++ resources/js/pages/catalogoCuentas/Index.vue
@@ -1,8 +1,38 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
-import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+interface CuentaPadre {
+    id: number,
+    codigo: string,
+    cuenta: string,
+    cuenta_padre_id: number|null,
+    esta_activo: boolean
+}
+
+interface Cuentas {
+    id: number,
+    codigo: string,
+    cuenta: string,
+    cuenta_padre_id: number|null,
+    esta_activo: boolean,
+    cuenta_padre?: CuentaPadre
+}
+
+interface Props {
+    cuentas: Cuentas[]
+}
+
+const props = defineProps<Props>();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,6 +48,29 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+
+            <h1 class="font-bold">Listado de Catalogo de Cuentas</h1>
+
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">#</TableHead>
+        <TableHead class="w-[100px]">Código</TableHead>
+        <TableHead>Cuenta</TableHead>
+        <TableHead>Cuenta Padre</TableHead>
+        <TableHead>Cuenta Activa</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="catalogo in props.cuentas" :key="catalogo.id">
+                        <TableCell>{{catalogo.id}}</TableCell>
+                        <TableCell>{{catalogo.codigo}}</TableCell>
+                        <TableCell>{{catalogo.cuenta}}</TableCell>
+                        <TableCell>{{catalogo.cuenta_padre?.cuenta ?? 'Sin cuenta padre'}}</TableCell>
+                        <TableCell>{{catalogo.esta_activo ? 'SI' : 'NO'}}</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
         </div>
     </AppLayout>
 </template>
